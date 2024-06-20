b9ee72865f6750101e8d74e9422be325d31bf653
linuxfight <kiko13024@gmail.com>
2024-06-20 19:16:32 +0300

    add default values for configuration

--- Program.cs
+++ Program.cs
@@ -5,19 +5,29 @@ var builder = WebApplication.CreateSlimBuilder(args);
 
 builder.Services.AddControllers();
 
-var jwtIssuer = builder.Configuration.GetSection("Jwt:Issuer").Get<string>();
-var jwtAudience = builder.Configuration.GetSection("Jwt:Audience").Get<string>();
-var jwtKey = Environment.GetEnvironmentVariable("RANDOM_SECRET");
-var pgHost = Environment.GetEnvironmentVariable("POSTGRES_HOST");
-var pgUser = Environment.GetEnvironmentVariable("POSTGRES_USERNAME");
-var pgPass = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
-var pgPort = Environment.GetEnvironmentVariable("POSTGRES_PORT");
-var pgDb = Environment.GetEnvironmentVariable("POSTGRES_DATABASE");
-var serverAddress = Environment.GetEnvironmentVariable("SERVER_ADDRESS");
+var jwtIssuer = builder.Configuration.GetSection("Jwt:Issuer").Get<string>() ?? "prodapi";
+var jwtAudience = builder.Configuration.GetSection("Jwt:Audience").Get<string>() ?? "prodapi";
+var jwtKey = Environment.GetEnvironmentVariable("RANDOM_SECRET") ?? "veryrandomsecret";
+var pgHost = Environment.GetEnvironmentVariable("POSTGRES_HOST") ?? "localhost";
+var pgUser = Environment.GetEnvironmentVariable("POSTGRES_USERNAME") ?? "user";
+var pgPass = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD") ?? "1234567890";
+var pgPort = Environment.GetEnvironmentVariable("POSTGRES_PORT") ?? "5432";
+var pgDb = Environment.GetEnvironmentVariable("POSTGRES_DATABASE") ?? "db";
+var serverAddress = Environment.GetEnvironmentVariable("SERVER_ADDRESS") ?? "0.0.0.0:8080";
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql($"Host={pgHost};Port={pgPort};Database={pgDb};Username={pgUser};Password={pgPass}"));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    using (var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>())
+    {
+        var migrations = await context.Database.GetPendingMigrationsAsync();
+        if (migrations.Any())
+            await context.Database.MigrateAsync();
+    }
+}
 
 app.Urls.Add($"http://{serverAddress}");
 app.MapControllers();
